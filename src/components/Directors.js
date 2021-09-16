f63dd177ef940194815e7cde43b61924ac596e3d
Karen Olson <karmarie@gmail.com>
2021-09-16 14:06:01 -0500

    commit tree fb9cc8dc16b2ef9d989a585ad07abe3cc5b4d9c4

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { directors } from "../data";
+import { nanoid } from "nanoid";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorCards = directors.map((director) => {
+    const movieList = director.movies.map((movie) => (
+      <li key={nanoid()}>{movie}</li>
+    ));
+    return (
+      <div key={nanoid()}>
+        <h2>{director.name}</h2>
+        <ul>Movies: {movieList}</ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorCards}
+    </div>
+  );
 }
 
 export default Directors;
