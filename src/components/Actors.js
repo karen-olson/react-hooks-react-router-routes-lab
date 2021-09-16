6232605bae56d3e439b6800c302a6fd493a2bea8
Karen Olson <karmarie@gmail.com>
2021-09-16 14:06:09 -0500

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { actors } from "../data";
+import { nanoid } from "nanoid";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorCards = actors.map((actor) => {
+    const movieCards = actor.movies.map((movie) => (
+      <li key={nanoid()}>{movie}</li>
+    ));
+    return (
+      <div key={nanoid()}>
+        <h2>{actor.name}</h2>
+        {movieCards}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorCards}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { movies } from "../data";
+import { nanoid } from "nanoid";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieCards = movies.map((movie) => {
+    const movieGenres = movie.genres.map((genre) => (
+      <li key={nanoid()}>{genre}</li>
+    ));
+    return (
+      <div key={nanoid()}>
+        <h2>{movie.title}</h2>
+        <h4>Runtime: {movie.time} minutes</h4>
+        <ul>Genres: {movieGenres}</ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieCards}
+    </div>
+  );
 }
 
 export default Movies;
